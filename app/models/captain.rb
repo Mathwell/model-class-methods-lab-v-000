5803305337e6651a9322220c1e286e3ff28b137d
Olena Ageyeva <eageeva@hotmail.com>
2018-02-02 01:17:42 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,7 +1,7 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-  def catamaran_operators
+   def self.catamaran_operators
     includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 end
